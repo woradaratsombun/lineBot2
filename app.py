2b96c3e2bcaeacb1c87d637426d5fdfefcc5f6f3
woradaratsombun <51810171+woradaratsombun@users.noreply.github.com>
2019-10-09 10:51:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,16 +22,14 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    #sendText(user,userText)
     if (userText == 'สวัสดี') :
         sendText(user,'เออออว่ะ')
     elif (userText == 'ไปละ') :
         sendText(user,('ไปไหนก็ไป ไป๊')
     else :
         sendText(user,('ว่าไงนะ')
-    
     return '',200
-
+   
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
